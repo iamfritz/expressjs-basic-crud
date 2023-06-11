6478cde8b7780bd9e3cdc46c7709d53e83bd46cc
fritz-darryl <link4anything@yahoo.com>
2023-06-11 16:42:40 +0800

    basic api crud

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # expressjs-basic-crud
+
+
+npm i express mongoose nodemon dotenv
---
+++ app.js
@@ -0,0 +1,33 @@
+//https://www.freecodecamp.org/news/build-a-restful-api-using-node-express-and-mongodb/
+
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const router = express.Router();
+const routes = require("./routes/routes");
+
+module.exports = router;
+
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log('DB error');
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+const app = express();
+
+app.use(express.json());
+
+app.use("/api", routes);
+
+app.listen(3000, () => {
+  console.log(`Server Started at ${3000}`);
+});
