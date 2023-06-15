f85a6c9c75930885aaa6aad208511df587623e3a
fritz-darryl <link4anything@yahoo.com>
2023-06-16 02:08:25 +0800

    create registration and login endpoint

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ const lib = require("../app/middleware/lib");
 const router  = express.Router();
 const User = require("../app/models/user");
 const authenticator = require("../app/middleware/authenticator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = router;
 
@@ -60,6 +62,21 @@ router.patch("/update/:id", async (req, res) => {
     const updatedData = req.body;
 
     const options = { new: true };
+    if (req.body.password) {
+      req.body.password = await bcrypt
+        .hash(req.body.password, 10)
+        .then((hashedPassword) => {
+          return hashedPassword;
+        })
+        // catch error if the password hash isn't successful
+        .catch((e) => {
+          response.status(500).send({
+            message: "Password was not hashed successfully",
+            e,
+          });
+        });      
+      
+    }
 
     const result = await User.findByIdAndUpdate(id, updatedData, options);
 
@@ -81,6 +98,109 @@ router.delete("/delete/:id", async (req, res) => {
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
+});
+
+// register endpoint
+router.post("/register", (request, response) => {
+  // hash the password     
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+        name: request.body.name,
+        age: request.body.age,
+        position: request.body.position,
+        level: request.body.level        
+      });
+
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+
+});
+
+// login endpoint
+router.post("/login", (request, response) => {
+
+  if (!request.body.email || !request.body.password) {
+    response.status(400).json({ error: "Email Address and Password is required." });
+  }
+    // check if email exists
+    User.findOne({ email: request.body.email })
+
+      // if email exists
+      .then((user) => {
+        // compare the password entered and the hashed password found
+        bcrypt
+          .compare(request.body.password, user.password)
+
+          // if the passwords match
+          .then((passwordCheck) => {
+            // check if password matches
+            if (!passwordCheck) {
+              return response.status(400).send({
+                message: "Passwords does not match",
+                error,
+              });
+            }
+
+            //   create JWT token
+            const token = jwt.sign(
+              {
+                userId: user._id,
+                userEmail: user.email,
+              },
+              "RANDOM-TOKEN",
+              { expiresIn: "24h" }
+            );
+
+            //   return success response
+            response.status(200).send({
+              message: "Login Successful",
+              email: user.email,
+              token,
+            });
+          })
+          // catch error if password does not match
+          .catch((error) => {
+            response.status(400).send({
+              message: "Passwords does not match",
+              error,
+            });
+          });
+      })
+      // catch error if email does not exist
+      .catch((e) => {
+        response.status(404).send({
+          message: "Email not found",
+          e,
+        });
+      });
 });
 
 router.use((req, res, next) => {
